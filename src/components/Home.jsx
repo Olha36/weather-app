2671cd8e471eea78a152bbeb3ec70cf2689362df
olha36 <olhakucheruk36@gmail.com>
2025-09-25 14:38:41 +0300

    style search block

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
+import WeatherSearch from "./WeatherSearch";
 
 export default function Home() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Welcome to Home Page</h1>
-    </div>
+    <>
+      <WeatherSearch
+     
+      />
+    </>
   );
 }
